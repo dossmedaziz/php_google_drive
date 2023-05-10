655a83671609a4ebb050022983c09e20950fbb3e
dossmedaziz <dossaziz4@gmail.com>
2023-05-10 11:50:00 +0100

    pass the folder id from cli

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/GoogleClient.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-// check if file is passed as argument
 try {
     if (!isset($argv[1])) {
         throw new Exception("File Path is missing\n");
     }
+    if (!isset($argv[2])) {
+        throw new Exception("Folder Id is missing\n");
+    }
     $googleClient = GoogleClient::getInstance();
     $googleClient->setGoogleCredentials(__DIR__ . '/credentials.json');
-    $googleClient->setFolderId($_ENV['FOLDER_ID']);
+    $googleClient->setFolderId($argv[2]);
     $googleClient->upload([
         'file' => $argv[1],
         'name' => basename($argv[1])
